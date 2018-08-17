e2b8643fa3590672783813a667de23e509acf1a1
Tmbjayanath <jayanathtmbj@gmail.com>
2018-08-17 21:42:44 +0530

    Emit  newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function () {
   console.log('connected to server');
-
-  socket.emit('createMessage', {
-    from: 'Rush',
-    text: 'hey, thats work for me',
-  });
 });
 
 socket.on('disconnect', function () {
